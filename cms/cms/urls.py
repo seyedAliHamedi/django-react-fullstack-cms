b47a863839020b91d7fd3a34c6d54ef31597c77e
hamediseyedali <hamediseyedali@gmail.com>
2024-08-10 05:22:25 +0330

    frontend connected

--- cms/cms/urls.py
+++ cms/cms/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('blog.urls')),
+    path('frontend/', include('frontend.urls')),
 ]
